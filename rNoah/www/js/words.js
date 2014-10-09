1b2da87816284f4ca7df17bb3656fc9800414c79
HeffDev <email@heffdev.com>
2014-10-09 19:56:38 +0200

    sounds files and words.js

---
+++ rNoah/www/js/words.js
@@ -0,0 +1,104 @@
+var words = {
+		"word" : [
+		          	{
+		          		"letters" : "C,A,R",
+		          		"theWord" : "CAR",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "B,O,A,T",
+		          		"theWord" : "BOAT",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "T,R,E,E",
+		          		"theWord" : "TREE",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "T,R,A,I,N",
+		          		"theWord" : "TRAIN",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "B,O,T,T,L,E",
+		          		"theWord" : "BOTTLE",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "T,A,B,L,E",
+		          		"theWord" : "TABLE",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "H,A,N,D",
+		          		"theWord" : "HAND",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "B,I,R,D",
+		          		"theWord" : "BIRD",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "R,U,N",
+		          		"theWord" : "RUN",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "D,O,G",
+		          		"theWord" : "DOG",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "C,A,T",
+		          		"theWord" : "CAT",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "H,O,U,R,S,E",
+		          		"theWord" : "HOURSE",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "H,O,U,S,E",
+		          		"theWord" : "HOUSE",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "B,O,O,K",
+		          		"theWord" : "BOOK",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "F,L,Y",
+		          		"theWord" : "FLY",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "W,A,T,E,R",
+		          		"theWord" : "WATER",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "B,A,L,L",
+		          		"theWord" : "BALL",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "C,L,O,C,K",
+		          		"theWord" : "CLOCK",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "C,U,P",
+		          		"theWord" : "CUP",
+		          		"longLetters" : ""
+		          	},
+		          	{
+		          		"letters" : "G,L,A,S,S",
+		          		"theWord" : "GLASS",
+		          		"longLetters" : ""
+		          	},
+		          ]
+}
